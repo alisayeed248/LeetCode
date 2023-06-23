74c1beb8a2c643b32ab47f698d7499f09bb7a3f6
Sayeed Ali <88571614+alisayeed248@users.noreply.github.com>
2023-06-22 22:27:00 -0400

    Added tail implementation instead of head

--- 155. Min Stack.java
+++ 155. Min Stack.java
@@ -1,4 +1,58 @@
 // Credit to https://leetcode.com/problems/min-stack/solutions/49010/clean-6ms-java-solution/
+// Tail variation
+
+class MinStack {
+    private class Node {
+        // 10 min
+        // (9, 9) --> (10, 9) --> (10, 8) min --> (10, 7)
+        // 10 --> 8 --> 12 
+        private int val;
+        private int min;
+        private Node prev;
+
+        public Node(int val, int min, Node prev) {
+            this.val = val;
+            this.min = min;
+            this.prev = prev;
+        }
+    }
+
+    private Node tail;
+
+    public MinStack() {
+    }
+
+    public void push(int val) {
+        if (tail == null) {
+            tail = new Node(val, val, null);
+        }
+        else {
+            tail = new Node(val, Math.min(val, tail.min), tail);
+        }
+    }
+
+    public void pop() {
+        tail = tail.prev;
+    }
+    
+    public int top() {
+        return tail.val;
+    }
+
+    public int getMin() {
+        return tail.min;
+    }
+
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
 
 class MinStack {
     private class Node {
